ba55e088789e05048b5a0b0380d6193164077983
Armin Braun <me@obrown.io>
2016-08-04 15:00:59 +0200

    Update run.sh

--- pdl/run.sh
+++ pdl/run.sh
@@ -3,6 +3,8 @@ cat ~/.ssh/id_dsa.pub >> ~/.ssh/authorized_keys
 echo -e 'Host localhost *\n\tStrictHostKeyChecking no\n' >> ~/.ssh/config
 sudo service ssh start
 
-export MAVEN_OPTS=-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
-mvn clean install -B -Pqulice --settings settings.xml
-mvn clean
+if [ -e settings.xml ]; then
+  export MAVEN_OPTS=-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
+  mvn clean install -B -Pqulice --settings settings.xml
+  mvn clean
+fi
